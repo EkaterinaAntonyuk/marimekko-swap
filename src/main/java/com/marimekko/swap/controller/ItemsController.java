dca14528d68b7a6bcdde6d002f44f94c3ea4ab3d
Nikita Verkhovin <verkhovin13@gmail.com>
2021-11-20 23:11:01 +0200

    urls and cors

--- src/main/java/com/marimekko/swap/controller/ItemsController.java
+++ src/main/java/com/marimekko/swap/controller/ItemsController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class ItemsController {
     private final ItemsService itemsService;
     private final ProfileService profileService;
